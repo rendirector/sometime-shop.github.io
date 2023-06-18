301d3cbd68a3578cfc49389010871d9fadaf6371
rendirector <125809844+rendirector@users.noreply.github.com>
2023-06-18 15:06:10 +0700

    beta
    test

---
+++ include/Config.php
@@ -0,0 +1,10 @@
+<?php
+// MySQL Database
+$_cfg['host'] = 'localhost';
+$_cfg['user'] = 'root';
+$_cfg['pass'] = '';
+$_cfg['dbnm'] = 'shop';
+$wallet['phone'] = "0809878451"; //เบอร์วอลเล็ต
+$wallet['name'] = "asdasda"; //ชื่อในวอลเล็ต
+$wallet['!'] = "sadsd"; //เอกลักษณ์ รูปโปรไฟล์ของวอเล็ต
+?>
---
+++ template/Config.php
@@ -0,0 +1,10 @@
+<?php
+// MySQL Database
+$_cfg['host'] = 'localhost';
+$_cfg['user'] = 'hshopsxy_shop';
+$_cfg['pass'] = 'xEyK2CWdj';
+$_cfg['dbnm'] = 'hshopsxy_shop';
+$wallet['phone'] = "0809878451"; //เบอร์วอลเล็ต
+$wallet['name'] = "วโรดม เตซะฟอง"; //ชื่อในวอลเล็ต
+$wallet['!'] = "เป็นรูปหมา"; //เอกลักษณ์ รูปโปรไฟล์ของวอเล็ต
+?>
